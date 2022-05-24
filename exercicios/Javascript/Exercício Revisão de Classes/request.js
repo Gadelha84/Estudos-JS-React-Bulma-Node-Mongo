87e1354a731189b826ccfad92ab9abaebd6186b1
Paulo Gadelha <pc.gadelha@hotmail.com>
2022-05-24 17:13:28 -0300

    new exercises commit
    Commit de exercicios de fixação de classes

--- exercicios/Javascript/Exercício Revisão de Classes/request.js
+++
@@ -1,7 +0,0 @@
-export default class spaceship {
-    
-    constructor(name, crewQuantity) {
-        this.name = prompt("Informe o nome da nave: ");
-        this.crewQuantity = prompt(parseInt("Informe a quantidade de tripulantes: "));
-        }
-}
