c7b2aa63db1004c09d6d996ea1846822e93b6a74
Paulo Gadelha <pc.gadelha@hotmail.com>
2022-06-22 11:57:00 -0300

    commit React
    commit de aulas React

---
+++ exercicios/React/production/my-fisrt-app/src/components/LoremIpsum.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Lorem = () =>{
+    return <p>Mussum Ipsum, cacilds vidis litro abertis. Quem num gosta di mé, boa gentis num é. Quem num gosta di mim que vai caçá sua turmis! Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis. Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis.</p>
+}
+
+export default Lorem
---
+++ exercicios/React/production/my-fisrt-app/src/components/planets/index.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from "react";
+import Planet from "./planet";
+
+const Planets = () => {
+    return (
+        <Fragment>
+            <h3>Planet List</h3>
+            <hr/>
+            <Planet name="Mercúrio"
+                    description="Mercúrio é o menor[nota 1][nota 2] e mais interno planeta do Sistema Solar, orbitando o Sol a cada 87,969 dias terrestres. A sua órbita tem a maior excentricidade e o seu eixo apresenta a menor inclinação em relação ao plano da órbita dentre todos os planetas do Sistema Solar. Mercúrio completa três rotações em torno de seu eixo a cada duas órbitas."
+                    img_url="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/280px-Mercury_in_color_-_Prockter07-edit1.jpg"
+            />
+
+<Planet name="Vênus"
+                    description="Vénus ou Vênus é o segundo planeta do Sistema Solar em ordem de distância a partir do Sol, orbitando-o a cada 224,7 dias. Recebeu seu nome em homenagem à deusa romana do amor e da beleza Vénus, equivalente a Afrodite. Depois da Lua, é o objeto mais brilhante do céu noturno, atingindo uma magnitude aparente de -4,6, o suficiente para produzir sombras. A distância média da Terra a Vênus é de 0,28 AU, sendo esta a menor distância entre qualquer par de planetas."
+                    img_url="https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/PIA23791-Venus-RealAndEnhancedContrastViews-20200608_%28cropped2%29.jpg/280px-PIA23791-Venus-RealAndEnhancedContrastViews-20200608_%28cropped2%29.jpg"
+            />
+        </Fragment>
+    )
+}
+
+export default Planets;
